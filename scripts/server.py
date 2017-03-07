a1513c50281e10938b0a94c2fb4e2edf44760805
ipa-lth <lth.ipa.fhg.de>
2017-03-07 15:53:29 +0100

    first useful prototype

--- scripts/server.py
+++ scripts/server.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 
 from bottle import route, run, template
-from bottle import get, static_file
+from bottle import static_file
+
+from os import listdir
+from os.path import isfile, join
 
 @route('/hello/<name>')
 def index(name):
@@ -11,12 +14,11 @@ def index(name):
 def send_image(filename):
     return static_file(filename, root='images', mimetype='image/png')
 
-
 @route('/<name>')
 def index(name):
     return template('\
-    	<b>Hello {{name}}</b>!<br>\
-    	<video width="800" height="600" controls>\
+        <h3>{{name}}</h3><br>\
+        <video width="800" height="600" controls>\
           <source src="videos/{{name}}" type=\'video/mp4\'>\
         Your browser does not support the video tag.\
         </video> ',
@@ -24,8 +26,15 @@ def index(name):
 
 @route('/videos/<filename:re:.*\.mp4>')
 def send_video(filename):
-	return static_file(filename, root='videos', mimetype='video/mp4')
+    return static_file(filename, root='videos', mimetype='video/mp4')
 
+@route('/files')
+def get_files():
+    onlyfiles = [f for f in listdir('videos') if isfile(join('videos', f))]
+    result='<h1>All available files in folder</h1><br>'
+    for file1 in onlyfiles:
+        result +=  '<a href="{}">{}</a><br>'.format(file1, file1)
+    return result
 
-#run(host='0.0.0.0', port=8080, debug=True)
-run(host='localhost', port=8080, debug=True)
+run(host='0.0.0.0', port=8080, debug=True)
+#run(host='localhost', port=8080, debug=True)
