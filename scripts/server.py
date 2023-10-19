f83ab9277f2ca2d69608e42148acd8a2fde6dadf
Lorenz Halt <lorenz.halt@ipa.fraunhofer.de>
2023-10-19 11:15:16 +0200

    update to python 3.12

--- scripts/server.py
+++ scripts/server.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python
 
 from bottle import route, run, template, static_file, request
+from collections import defaultdict
 import os
 from os import walk
 
-from thread import start_new_thread
 import subprocess
 import hashlib
 
 _video_folder = '.'
 
-_ffmpeg_folder = 'h:\\'
 
 def gethtml(title, content, refresh=None, redirect=None):
     ans = '<html> \
@@ -35,198 +34,231 @@ def gethtml(title, content, refresh=None, redirect=None):
         '</html>'
     return ans
 
+def gethtml5(title, content, refresh=None, redirect=None):
+    ans = '''
+    <!doctype html>
+    <html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        ''' 
+    # if refresh:
+    #     ans += '<meta http-equiv="refresh" content="{};'.format(refresh)
+    #     if redirect:
+    #         ans += 'url={}" />'.format(redirect)
+    # ans += '"/>'
+    ans += '''
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+        <title>{}</title>
+    </head>
+    <body>
+    '''.format(title) + \
+    '{}'.format(content) + \
+    '''
+        <!-- Optional JavaScript; choose one of the two! -->
+
+        <!-- Option 1: Bootstrap Bundle with Popper -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+        <!-- Option 2: Separate Popper and Bootstrap JS -->
+        <!--
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+        -->
+    </body>
+    </html>
+    '''
+    return ans
+
 @route('/')
 def overview():
     content =  '<h1>Overview</h1><br>'+\
                 '<div class="list-group">\
+                  <a href="/crawl" class="list-group-item">Crawl</a>\
                   <a href="/all" class="list-group-item active">See all videos</a>\
-                  <a href="/shutdown" class="list-group-item">Shut down server</a>\
-                  <a href="/shutdown/abort" class="list-group-item">Abort shutdown</a>\
+                  <a href="/exit" class="list-group-item">Shut down server</a>\
                 </div>'
 
     return gethtml("Overview", content)
 
+def crawl(video_folder):
+    webvideo_files = []
+    webvideo_links = []
+    for (dirpath, dirnames, filenames) in os.walk(video_folder):
+        webvideo_files.extend([file for file in filenames if file.endswith(('.mp4', '.ogg', '.webm'))])
+        webvideo_links.extend(['/video/{}?path={}'.format(i, dirpath) for i in webvideo_files])
+    return webvideo_files, webvideo_links
+
+def create_link(file_name, file_path, file_type):
+    file_link = file_type
+    if file_type == 'MP4':
+        file_link = '/video/{}?path={}'.format(file_name, file_path)
+    elif file_type == 'PNG':
+        file_link = '/image/{}?path={}'.format(file_name, file_path)
+    elif file_type == 'JPG' or 'JPEG':
+        file_link = '/image/{}?path={}'.format(file_name, file_path)
+    return file_link
+
+def crawl_local_path(path):
+    extensions = ['.mp4', '.png', '.jpg', '.jpeg']
+    files = []
+
+    for root, _, filenames in os.walk(path):
+        for filename in filenames:
+            file_extension = os.path.splitext(filename)[1].lower()
+            if file_extension in extensions:
+                file_path = root
+                file_type = file_extension[1:].upper()
+                files.append({
+                    'name': filename,
+                    'path': file_path,
+                    'type': file_type,
+                    'link': create_link(filename, file_path, file_type),
+                })
+    sorted_files = sorted(files, key=lambda x: (x['path'], x['name']))
+    return sorted_files
+
+def store(webvideo_files, webvideo_links, filename):
+    with open(filename, 'w') as file:
+        file.write('\n'.join(webvideo_files) + '\n')
+        file.write('\n'.join(webvideo_links) + '\n')
+        checksum = hashlib.md5('\n'.join(webvideo_files + webvideo_links).encode()).hexdigest()
+        file.write(checksum)
+
+def load(filename):
+    webvideo_files = []
+    webvideo_links = []
+    stored_checksum = ''
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+        webvideo_files = lines[0].strip().split('\n')
+        webvideo_links = lines[1].strip().split('\n')
+        stored_checksum = lines[2].strip()
+
+    computed_checksum = hashlib.md5('\n'.join(webvideo_files + webvideo_links).encode()).hexdigest()
+    if stored_checksum == computed_checksum:
+        return webvideo_files, webvideo_links
+    else:
+        raise ValueError('Checksum mismatch. The stored file may have been modified.')
+
+@route('/crawl')
+def cache():
+    webvideo_files, webvideo_links = crawl(_video_folder)
+    store(webvideo_files, webvideo_links, "_cache.uflix")
+    return
+
 @route('/all')
 def get_files():
-    content =   '<h1>All Videos</h1><br>'+\
+    content =   '<h1>All Files</h1><br>'+\
                     '<div class="panel-group">'
 
-    compile_files = []
-    for (dirpath, dirnames, filenames) in walk(_video_folder):
-        webvideo_files = [file for file in filenames if (file.endswith(
-            ('.mp4','.ogg', '.webm')
-        ) and
-            not file.startswith("_")
-        )]
-        webvideo_links = ['{}?path={}'.format(i, dirpath) for i in webvideo_files]
-        compile_files.extend([file for file in filenames if (file.endswith('.mp4') and
-            file.startswith("_"))])
+    files = crawl_local_path(_video_folder)
+    # try:
+    #     webvideo_files, webvideo_links = load("_cache.uflix")
+    # except:
+    #     webvideo_files, webvideo_links = crawl(_video_folder)
+    #     store(webvideo_files, webvideo_links, "_cache.uflix")
 
-        avi_files = [file for file in filenames if file.endswith(('.avi', '.mkv'))]
-        avi_files_tmp = [file for file in filenames if file.endswith(('.avi', '.mkv'))]
-        
-        avi_links = ['/convert/{}?path={}'.format(avi, dirpath) for avi in avi_files]
-        compile_cnt = 0
-        for avi in avi_files_tmp: #Use a inplace copy to iterate to avoid multiple arrays
-            if (('{}.mp4'.format(avi) in webvideo_files) or ('_{}.mp4'.format(avi) in compile_files)):
-                #print avi
-                avi_files.remove(avi)
-                avi_links.remove('/convert/{}?path={}'.format(avi, dirpath))
-                if '_{}.mp4'.format(avi) in compile_files:
-                    compile_cnt += 1
-        #    else:
-        #        avi_links.extend(['/convert/{}?path={}'.format(avi, dirpath)])
+    # for (dirpath, dirnames, filenames) in walk(_video_folder):
+    #     webvideo_files = [file for file in filenames if file.endswith(('.mp4','.ogg', '.webm'))] # not  '.avi', '.mkv'
+    #     webvideo_links = ['/video/{}?path={}'.format(i, dirpath) for i in webvideo_files]
 
-        # create html here
-        if (len(webvideo_files) + len(avi_files)) != 0:
-            m = hashlib.md5()
-            m.update(dirpath)
-            hashfrompath = m.hexdigest()
-            content +=  '<div class="panel panel-default">\
-                            <a class="list-group-item" data-toggle="collapse" href="#{}">'.format(hashfrompath)+\
-                                '<span class="badge progress-bar-warning">{}</span>'.format(compile_cnt)+\
-                                '<span class="badge progress-bar-danger">{}</span>'.format(len(avi_files))+\
-                                '<span class="badge progress-bar-info">{}</span>'.format(len(webvideo_files))+\
-                                "{}".format(dirpath)+\
-                            '</a>'+\
-                            '<div class="panel-collapse collapse" id="{}">'.format(hashfrompath)+\
-                                '<div class="panel-body">'
-            for vid, link in zip(webvideo_files, webvideo_links):
-                content += '<a href="{}" class="list-group-item active">{}</a>'.format(link, vid)
+    # create html here
 
-            for avi, link in zip(avi_files, avi_links):
-                content += '<a href="{}" class="list-group-item">{}</a>'.format(link, avi)
+    path_counts = defaultdict(int)
 
-            content +=          '</div>\
+    for file in files:
+        path_counts[file['path']] += 1
+
+    for path, count in path_counts.items():
+        m = hashlib.md5()
+        m.update(path.encode('utf-8'))
+        hashfrompath = m.hexdigest()
+        content +=  '<div class="panel panel-default">\
+                        <a class="list-group-item" data-toggle="collapse" href="#{}">'.format(hashfrompath)+\
+                                '<span class="badge progress-bar-info">{}</span>'.format(count)+\
+                            "{}".format(path)+\
+                        '</a>'+\
+                        '<div class="panel-collapse collapse" id="{}">'.format(hashfrompath)+\
+                            '<div class="panel-body">'
+        for file in files:
+            if file['path'] == path:
+                content += '<a href="{}" class="list-group-item">{}</a>'.format(file['link'], file['name'])
+        content +=               '</div>\
                             </div>\
                         </div>\
                     </div>\
                 </div>'
+
     return gethtml("All files",
                content)
 
-@route('/all-medialist')
-def get_files():
-    result='<h1>All available files</h1><br>'+\
-           '<div class="vids">\
-                    <ul class="media-list">'
 
-    for (dirpath, dirnames, filenames) in walk(_video_folder):
-        webvideo_files = [file for file in filenames if (file.endswith(
-            ('.mp4','.ogg', '.mkv', '.webm')
-        ) and
-            not file.startswith("_")
-        )]
-        webvideo_links = ['{}?path={}'.format(i, dirpath) for i in webvideo_files]
+# @route('/all-medialist')
+# def get_files():
+#     result = '<h1>All available files</h1><br>' + \
+#              '<div class="vids">' + \
+#              '<ul class="media-list">'
 
-        avi_files = [file for file in filenames if file.endswith('.avi')]
-        avi_links = []
-        for avi in avi_files:
-            if not "{}.mp4".format(avi) in webvideo_files:
-                avi_links.extend(['/convert/{}?path={}'.format(avi, dirpath)])
-            else:
-                avi_files.remove(avi)
+#     for (dirpath, dirnames, filenames) in walk(_video_folder):
+#         webvideo_files = [file for file in filenames if file.endswith(('.mp4', '.ogg', '.mkv', '.webm'))]
+#         print(webvideo_files)
+#         webvideo_links = ['/video/{}?path={}'.format(i, dirpath) for i in webvideo_files]
+#         print(webvideo_links)
 
-        # create html here
-        if len(webvideo_files) != 0 and len(avi_files) != 0:
-            result += '<li class="media">\
-                            <a class="pull-left" href="#"> \
-                                <span class="glyphicon glyphicon-folder-open" \
-                                      style="width: 64px; height: 64px;" \
-                                      aria-hidden="true">\
-                                </span>\
-                                <!--img class="media-object" alt="[[img alt tag]]" style="width: 20px; height: 20px;" src="[[img url]]" /-->\
-                            </a>\
-                            <div class="media-body">'+\
-                                '<h4 class="media-heading">{}</h4>'.format(dirpath)
+#         if len(webvideo_files) != 0:
+#             result += f'<li class="media"> \
+#                             <a class="pull-left" href="#"> \
+#                                 <span class="glyphicon glyphicon-folder-open" \
+#                                       style="width: 64px; height: 64px;" \
+#                                       aria-hidden="true"> \
+#                                 </span> \
+#                             </a> \
+#                             <div class="media-body"> \
+#                                 <h4 class="media-heading">{dirpath}</h4>'
 
-            # Nested media object
-            for vid, link in zip(webvideo_files, webvideo_links):
-                result +=   '<div class="media">\
-                                  <a class="pull-left pull-middle" href="{}">'.format(link) +\
-                                      '<span class="glyphicon glyphicon-play" \
-                                            style="width: 64px; height: 64px;" \
-                                            aria-hidden="true">\
-                                      </span>\
-                                      <!--img class="media-object" alt="[[img alt tag]]" style="width: 64px; height: 64px;" src="[[img url]]" /-->\
-                                  </a>\
-                                  <div class="media-body">'+\
-                                  '<h4 class="media-heading">{}</h4>'.format(vid)+\
-                                  '</div>\
-                              </div>'
+#             for vid, link in zip(webvideo_files, webvideo_links):
+#                 result += f'<div class="media"> \
+#                                 <a class="pull-left pull-middle" href="{link}"> \
+#                                     <span class="glyphicon glyphicon-play" \
+#                                           style="width: 64px; height: 64px;" \
+#                                           aria-hidden="true"> \
+#                                     </span> \
+#                                 </a> \
+#                                 <div class="media-body"> \
+#                                     <h4 class="media-heading">{vid}</h4> \
+#                                 </div> \
+#                             </div>'
 
-            for avi, link in zip(avi_files, avi_links):
-                result +=   '<div class="media">\
-                                  <a class="pull-left pull-middle" href="{}">'.format(link) +\
-                                      '<span class="glyphicon glyphicon-record" \
-                                            style="width: 64px; height: 64px;" \
-                                            aria-hidden="true">\
-                                      </span>\
-                                      <!--img class="media-object" alt="[[img alt tag]]" style="width: 64px; height: 64px;" src="[[img url]]" /-->\
-                                  </a>\
-                                  <div class="media-body">'+\
-                                  '<h4 class="media-heading">{}</h4>'.format(avi)+\
-                                  '</div>\
-                              </div>'
-            result +=     '</div>\
-                     </li>'
+#             result += '</div> \
+#                      </li>'
 
-    return gethtml("All files",
-                   result)
+#     return gethtml("All files", result)
 
-@route('/test')
-def medialist():
-    content = '<div class="vids">\
-                    <ul class="media-list">\
-                        <li class="media">\
-                            <a class="pull-left" href="#"> \
-                                <span class="glyphicon glyphicon-play" \
-                                      style="width: 64px; height: 64px;" \
-                                      aria-hidden="true">\
-                                </span>\
-                                <!--img class="media-object" alt="[[img alt tag]]" style="width: 64px; height: 64px;" src="[[img url]]" /-->\
-                            </a>\
-                            <div class="media-body">\
-                                <h4 class="media-heading">Media heading</h4>\
-                                <p>[[media caption]]</p>\
-                                    <!-- Nested media object -->\
-                                    <div class="media"><a class="pull-left" href="#"> <img class="media-object" alt="[[img alt tag]]" style="width: 64px; height: 64px;" src="[[img url]]" /> </a>\
-                                        <div class="media-body">\
-                                        <h4 class="media-heading">Nested media heading</h4>\
-                                        [[media caption]]</div>\
-                                    </div>\
-                            </div>\
-                        </li>\
-                    </ul>\
-                </div>'
-    return gethtml("", content)
-
-@route('/<name>')
+@route('/video/<name>')
 def index(name):
     path = request.query.path
     return template('\
         <h3>{{name}}</h3><br>\
         <video width="800" height="600" preload="auto" controls>\
-          <source src="vid/{{name}}?path={{path}}" type=\'video/mp4\'>\
-          <source src="vid/{{name}}?path={{path}}" type=\'video/ogg\'>\
-          <source src="vid/{{name}}?path={{path}}" type=\'video/webm\'>\
+          <source src="/video/play/{{name}}?path={{path}}" type=\'video/mp4\'>\
+          <source src="/video/play/{{name}}?path={{path}}" type=\'video/ogg\'>\
+          <source src="/video/play/{{name}}?path={{path}}" type=\'video/webm\'>\
           Your browser does not support the video tag.\
         </video> ',
     name=name,
     path=path)
 
-@route('/vid/<filename:path>')
+@route('/video/play/<filename:path>')
 def send_video(filename):
     path = request.query.path
     return static_file(filename, root=path)
 
-def compileThread(path, name):
-    ans = '{}ffmpeg -i "{}" '.format(_ffmpeg_folder, path+'\\'+name) + \
-        '-vcodec libx264 -pix_fmt yuv420p ' + \
-        '-profile:v baseline -preset slower -crf 18 ' + \
-        '-vf "scale=trunc(in_w/2)*2:trunc(in_h/2)*2" '+ \
-        '"_{}.mp4"'.format(name)
-    subprocess.call(ans)
+# Examples
 
     new_name = '{}.mp4'.format(name)
 
@@ -316,9 +348,9 @@ def hello(name):
     return template('<b>Hello {{name}}</b>!', name=name)
 
 #Utils
-@route('/shutdown')
-def shutdown():
-    subprocess.call(["shutdown", "/s"])
+@route('/exit')
+def exit():
+    os._exit(0)
     return "<h1>System is shutting down!</h1>"
 
 @route('/shutdown/abort')
