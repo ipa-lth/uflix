8ae7b0c14bb7c9c1e84821212f3cb84cdc58a53f
Lorenz Halt <lorenz.halt@ipa.fraunhofer.de>
2023-10-19 15:13:47 +0200

    looking for good album examples

--- scripts/server.py
+++ scripts/server.py
@@ -4,12 +4,10 @@ from bottle import route, run, template, static_file, request
 from collections import defaultdict
 import os
 from os import walk
-
 import subprocess
 import hashlib
 
-_video_folder = '.'
-
+_video_folder = '.\\images'
 
 def gethtml(title, content, refresh=None, redirect=None):
     ans = '<html> \
@@ -260,92 +258,149 @@ def send_video(filename):
 
 # Examples
 
-    new_name = '{}.mp4'.format(name)
-
-    os.rename(os.path.join(os.getcwd(), '_'+new_name),
-              os.path.join(os.getcwd(), path, new_name))
-
-@route('/convert/<name>')
-def convert(name):
-    path = request.query.path
-#    ans =  '{}ffmpeg -i "{}" '.format(_ffmpeg_folder, path+'\\'+name) + \
-#            '-vcodec libx264 -pix_fmt yuv420p ' + \
-#            '-movflags frag_keyframe+empty_moov ' + \
-#            '-profile:v baseline -preset slower -crf 18 ' + \
-#            '-vf "scale=trunc(in_w/2)*2:trunc(in_h/2)*2" '+ \
-#            '"{}.mp4"'.format(os.path.splitext(name)[0])
-    start_new_thread(compileThread, (path, name))
-    return gethtml("Conversion",
-                   '<h3>Converting {} @ {}<br>Returning in 10 sec</h3>'.format(name, path),
-                   10,
-                   '/all')
-
-#            '<html>' + \
-#                '<head>' + \
-#                  '<title>Convertion</title>' + \
-#                  '<meta http-equiv="refresh" content="2; url=/all" />' + \
-#                '</head>' + \
-#                '<body>' + \
-#                '<h3>Converting {} @ {}<br>Returning in 2 sec</h3>'.format(name, path) + \
-#                '</body>' + \
-#            '</html>'
-
-# Examples
-@route('/list')
-def list():
-    content = '<div class="list-group">\
-                  <a href="#" class="list-group-item">\
-                    Cras justo odio\
-                  </a>\
-                  <a href="#" class="list-group-item">Dapibus ac facilisis in</a>\
-                  <a href="#" class="list-group-item">Morbi leo risus</a>\
-                  <a href="#" class="list-group-item">Porta ac consectetur ac</a>\
-                  <a href="#" class="list-group-item">Vestibulum at eros</a>\
-                </div>'
-    return gethtml('list', content)
-
-@route('/panelgroup')
-def panel():
-    content =   '<h1>Bootstrap Panel with Collapsable Sub-List Group</h1>\
-                    <div class="panel-group">\
-                        \
-                        <div class="panel panel-default">\
-                            <a class="list-group-item" data-toggle="collapse" href="#collapseTC001A">\
-                                <span class="badge">4</span>\
-                                TC001A\
-                            </a>\
-                            <div class="panel-collapse collapse" id="collapseTC001A">\
-                                <div class="panel-body">\
-                                    <a href="#" class="list-group-item">4</a>\
-                                    <a href="#" class="list-group-item">3</a>\
-                                    <a href="#" class="list-group-item">2</a>\
-                                    <a href="#" class="list-group-item">1</a>\
-                                </div>\
-                            </div>\
-                        </div>\
-                        \
-                        <div class="panel panel-default">\
-                            <a class="list-group-item" data-toggle="collapse" href="#collapseTC002A">\
-                                <span class="badge">2</span>\
-                                TC002A\
-                            </a>\
-                            <div class="panel-collapse collapse" id="collapseTC002A">\
-                                <div class="panel-body">\
-                                    <a href="#" class="list-group-item">2</a>\
-                                    <a href="#" class="list-group-item">1</a>\
-                                </div>\
-                            </div>\
-                        </div>\
-                    </div>'
-    return gethtml('list', content)
-
-@route('/images/<filename:re:.*\.png>')
+@route('/image/<filename:re:.*\.png>')
 def send_image(filename):
-    return static_file(filename, root='images', mimetype='image/png')
+    path = request.query.path
+    return static_file(filename, root=path, mimetype='image/png')
+
+@route('/image/<filename:re:.*\.jpg>')
+def send_image(filename):
+    path = request.query.path
+    return static_file(filename, root=path, mimetype='image/jpg')
 
 @route('/hello/<name>')
 def hello(name):
     return template('<b>Hello {{name}}</b>!', name=name)
+
+def generate_lightbox_code(files):
+    html_code = '<div class="lightbox">\n'
+    html_code += '  <div class="row">\n'
+    
+    # Iterate over the files and create the HTML code for each image
+    for file in files:
+        html_code += '    <div class="col-lg-6">\n'
+        html_code += f'      <img src="{file["link"]}" data-mdb-img="{file["link"]}" alt="{file["name"]}" class="w-100 mb-2 mb-md-4 shadow-1-strong rounded" />\n'
+        html_code += '    </div>\n'
+    
+    html_code += '  </div>\n'
+    html_code += '</div>'
+    
+    return html_code
+
+def generate_html_gallery(files):
+    html_code = '<!-- Gallery -->\n'
+    html_code += '<div class="row">\n'
+    
+    # Iterate over the files and create the HTML code for each image
+    for file in files:
+        html_code += '  <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">\n'
+        html_code += f'    <img src="{file["link"]}" class="w-100 shadow-1-strong rounded mb-4" alt="{file["name"]}" />\n'
+        html_code += '  </div>\n'
+    
+    html_code += '</div>\n'
+    html_code += '<!-- Gallery -->'
+    
+    return html_code
+
+def generate_html_carousel(files):
+    html_code = '<!-- Carousel wrapper -->\n'
+    html_code += '<div id="carouselMultiItemExample" class="carousel slide carousel-dark text-center" data-mdb-ride="carousel">\n'
+    html_code += '  <!-- Controls -->\n'
+    html_code += '  <div class="d-flex justify-content-center mb-4">\n'
+    html_code += '    <button class="carousel-control-prev position-relative" type="button" data-mdb-target="#carouselMultiItemExample" data-mdb-slide="prev">\n'
+    html_code += '      <span class="carousel-control-prev-icon" aria-hidden="true"></span>\n'
+    html_code += '      <span class="visually-hidden">Previous</span>\n'
+    html_code += '    </button>\n'
+    html_code += '    <button class="carousel-control-next position-relative" type="button" data-mdb-target="#carouselMultiItemExample" data-mdb-slide="next">\n'
+    html_code += '      <span class="carousel-control-next-icon" aria-hidden="true"></span>\n'
+    html_code += '      <span class="visually-hidden">Next</span>\n'
+    html_code += '    </button>\n'
+    html_code += '  </div>\n'
+    html_code += '  <!-- Inner -->\n'
+    html_code += '  <div class="carousel-inner py-4">\n'
+
+    # Divide the files into groups of 3
+    groups = [files[i:i+3] for i in range(0, len(files), 3)]
+
+    # Iterate over the groups and create the HTML code for each carousel item
+    for i, group in enumerate(groups):
+        if i == 0:
+            html_code += '    <!-- Single item -->\n'
+            html_code += '    <div class="carousel-item active">\n'
+        else:
+            html_code += '    <!-- Single item -->\n'
+            html_code += '    <div class="carousel-item">\n'
+
+        html_code += '      <div class="container">\n'
+        html_code += '        <div class="row">\n'
+
+        # Iterate over the files in the group and create the HTML code for each card
+        for file in group:
+            html_code += '          <div class="col-lg-4">\n'
+            html_code += '            <div class="card">\n'
+            html_code += f'              <img src="{file["link"]}" class="card-img-top" alt="{file["name"]}" />\n'
+            html_code += '              <div class="card-body">\n'
+            html_code += f'                <h5 class="card-title">{file["name"]}</h5>\n'
+            html_code += f'                <p class="card-text">{file["path"]}</p>\n'
+            html_code += '                <a href="#!" class="btn btn-primary">Button</a>\n'
+            html_code += '              </div>\n'
+            html_code += '            </div>\n'
+            html_code += '          </div>\n'
+
+        html_code += '        </div>\n'
+        html_code += '      </div>\n'
+        html_code += '    </div>\n'
+
+    html_code += '  </div>\n'
+    html_code += '  <!-- Inner -->\n'
+    html_code += '</div>\n'
+    html_code += '<!-- Carousel wrapper -->'
+
+    return html_code
+
+@route('/light')
+def light():
+    files = crawl_local_path(_video_folder)
+    content = generate_html_gallery(files)
+    return gethtml5('title', content)
+
+@route('/carousel')
+def light():
+    files = crawl_local_path(_video_folder)
+    content = generate_html_carousel(files)
+    return gethtml5('carousel', content)
+
+@route('/lightbox')
+def lightbox():
+    files = crawl_local_path(_video_folder)
+    content ='\
+<div class="lightbox">\
+  <div class="row">\
+    <div class="col-lg-6">\
+      <img\
+        src="https://mdbcdn.b-cdn.net/img/Photos/Thumbnails/Slides/1.webp"\
+        data-mdb-img="https://mdbcdn.b-cdn.net/img/Photos/Slides/1.webp"\
+        alt="Table Full of Spices"\
+        class="w-100 mb-2 mb-md-4 shadow-1-strong rounded"\
+      />\
+      <img\
+        src="https://mdbcdn.b-cdn.net/img/Photos/Thumbnails/Square/1.webp"\
+        data-mdb-img="https://mdbcdn.b-cdn.net/img/Photos/Square/1.webp"\
+        alt="Coconut with Strawberries"\
+        class="w-100 shadow-1-strong rounded"\
+      />\
+    </div>\
+    <div class="col-lg-6">\
+      <img\
+        src="https://mdbcdn.b-cdn.net/img/Photos/Thumbnails/Vertical/1.webp"\
+        data-mdb-img="https://mdbcdn.b-cdn.net/img/Photos/Vertical/1.webp"\
+        alt="Dark Roast Iced Coffee"\
+        class="w-100 shadow-1-strong rounded"\
+      />\
+    </div>\
+  </div>\
+</div>'
+    return gethtml5("Lightbox", content)
 
 #Utils
 @route('/exit')
@@ -353,11 +408,3 @@ def exit():
     os._exit(0)
     return "<h1>System is shutting down!</h1>"
 
-@route('/shutdown/abort')
-def no_shutdown():
-    subprocess.call(["shutdown", "/a"])
-    return "<h1>Aborting shutdown!</h1>"
-
-print 'starting in:', os.getcwd()
-run(host='0.0.0.0', port=8080, debug=False)
-#run(host='localhost', port=8080, debug=True)
